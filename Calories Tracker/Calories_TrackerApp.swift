819392214574f0b5a6a5f81fbaf522f482dfa277
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-04-27 05:12:27 +0700

    Initial Commit

---
+++ Calories Tracker/Calories_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calories_TrackerApp.swift
+//  Calories Tracker
+//
+//  Created by Renzo Alvaroshan on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Calories_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
