21ae681c536e350aafd10d6b826432590c71300d
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-04-27 06:10:48 +0700

    Create Calories Tracker App

---
+++ Calories Tracker/Views/EditFoodView.swift
@@ -0,0 +1,44 @@
+//
+//  EditFoodView.swift
+//  Calories Tracker
+//
+//  Created by Renzo Alvaroshan on 27/04/22.
+//
+
+import SwiftUI
+
+struct EditFoodView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.dismiss) var dismiss
+    
+    var food: FetchedResults<Food>.Element
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("\(food.name!)", text: $name)
+                    .onAppear {
+                        name = food.name!
+                        calories = food.calories
+                    }
+                VStack {
+                    Text("Calories \(Int(calories))")
+                    Slider(value: $calories, in: 0...1000, step: 10)
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().editFood(food: food, name: name, calories: calories, context: managedObjectContext)
+                        dismiss()
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
